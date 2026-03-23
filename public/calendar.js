0a7733ad9fff0a20cd1d9a8550aebefe069900fc
JohnVettas <johnvettas7@gmail.com>
2026-03-23 23:50:59 +0200

    dynamic divs under semester

--- public/calendar.js
+++ public/calendar.js
@@ -1,69 +1,85 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      timeZone: 'UTC',
-      initialView: 'timeGridFiveDay',
-      locale: 'el',
-      slotMinTime:'08:00:00',
-      slotMaxTime:'21:00:00',
-      allDaySlot: false,
-      nowIndicator: true,
-      height: 'auto',
-      headerToolbar: {
-        left: '',
-        center: 'title',
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    timeZone: "UTC",
+    initialView: "timeGridFiveDay",
+    locale: "el",
+    slotMinTime: "08:00:00",
+    slotMaxTime: "21:00:00",
+    allDaySlot: false,
+    nowIndicator: true,
+    height: "auto",
+    headerToolbar: {
+      left: "",
+      center: "title",
+    },
+    views: {
+      timeGridFiveDay: {
+        type: "timeGrid",
+        duration: { days: 5 },
       },
-      views: {
-        timeGridFiveDay: {
-          type: 'timeGrid',
-          duration: { days: 5 },
-        }
-      },
-      events: 'subjects.json'
-    });
-  
-    calendar.render();
+    },
+    events: "subjects.json",
   });
 
+  calendar.render();
+});
 
+const buttons = document.querySelectorAll(".semesterButtons");
 
-const buttons = document.querySelectorAll('.semesterButtons')
+buttons.forEach((button) => {
+  let pressed = false;
 
-buttons.forEach(button => {
+  let sem = button.textContent[button.textContent.length - 1];
+  const SemesterDiv = document.getElementById(`Semester${sem}`);
 
-  button.onclick = function()
-  {
-    let sem = button.textContent[button.textContent.length - 1];
-    const SemesterDiv = document.getElementById(`Semester${sem}`);
-    
-    const div1 = document.createElement("div");
-    SemesterDiv.appendChild(div1);
-    div1.className = "course";
-    
-    const p1 = document.createElement("p");
-    p1.textContent = "Τεχνητή Νοημοσύνη";
-    div1.appendChild(p1);
+  const subjects = [
+    ["Ανάλυση", "Γραμμική", "Λογικη"],
+    ["Ανάλυση 2", "Java", "Αρχιτεκτονικές Υπολογιστών"],
+  ];
 
-    const checkbox1 = document.createElement("input");
-    checkbox1.type = "checkbox";
-    div1.appendChild(checkbox1);
-    checkbox1.className = "checkbox"
+  button.onclick = function () {
+    pressed = !pressed;
 
-  
+    if (pressed) {
+      for (let i = 0; i < subjects[sem - 1].length; i++) {
+        const div = document.createElement("div");
+        const p = document.createElement("p");
+        const checkbox = document.createElement("input");
+        // subjects[sem-1][i]
+        SemesterDiv.appendChild(div);
+        div.className = "course";
+        p.textContent = subjects[sem - 1][i];
+        div.appendChild(p);
+        checkbox.type = "checkbox";
+        div.appendChild(checkbox);
+        checkbox.className = "checkbox";
 
+		checkbox.onchange = function() {
+			if (this.checked) {
+				console.log(`${p.textContent} tick`);
+			} else {
+				console.log(`${p.textContent} xetick`);
+			}
+		};
 
+      }
 
-    const checkbox2 = document.createElement("checkbox");
-    const checkbox3 = document.createElement("checkbox");
-    const checkbox4 = document.createElement("checkbox");
-    const checkbox5 = document.createElement("checkbox");
-  
-    SemesterDiv.appendChild(checkbox2);
-    SemesterDiv.appendChild(checkbox3);
-    SemesterDiv.appendChild(checkbox4);
-    SemesterDiv.appendChild(checkbox5);
+      // SemesterDiv.appendChild(div1);
+      // div1.className = "course";
 
-  }
-  
-})
+      const checkbox2 = document.createElement("checkbox");
+      const checkbox3 = document.createElement("checkbox");
+      const checkbox4 = document.createElement("checkbox");
+      const checkbox5 = document.createElement("checkbox");
+
+      SemesterDiv.appendChild(checkbox2);
+      SemesterDiv.appendChild(checkbox3);
+      SemesterDiv.appendChild(checkbox4);
+      SemesterDiv.appendChild(checkbox5);
+    } else {
+      SemesterDiv.innerHTML = ``;
+    }
+  };
+});
