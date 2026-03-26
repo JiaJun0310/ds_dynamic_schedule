a144273cdc5ee540c43ea21a0a882109d9930705
vasilikimenagia-spec <vasilikimenagia@gmail.com>
2026-03-26 22:00:24 +0200

    translation to greek and css modification

--- public/calendar.js
+++ public/calendar.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 },
             },
             viewBtn:{
-                text: "Change Display",
+                text: "Αλλαγή Οθόνης",
                 click: function(){
                     hideList();
                 }
