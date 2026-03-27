20176fa5d05195ce450615b4a56328012918ae75
JohnVettas <johnvettas7@gmail.com>
2026-03-27 23:34:45 +0200

    working login screen with server side request and token

---
+++ public/login.js
@@ -0,0 +1,31 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault(); 
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://localhost:8000/login', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            
+            localStorage.setItem('token', result.token);
+            localStorage.setItem('username', result.username);
+
+            window.location.href = '/html/admin.html'; 
+        } else {
+            alert(result.message || "Σφάλμα σύνδεσης");
+        }
+    } catch (err) {
+        console.error("Login error:", err);
+        alert("Server error. Please try again later.");
+    }
+});
